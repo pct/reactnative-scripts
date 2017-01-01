53a13b6fa0707b95027415c82cf3009a0bc8d7e7
Jin-Sih Lin <linpct@gmail.com>
2017-01-01 14:19:50 +0800

    add hideNavBar to route

--- template/app/config/route.js
+++ template/app/config/route.js
@@ -9,7 +9,7 @@ import Item from '../components/item';
 export default class Route extends Component {
   render() {
     return(
-      <Router>
+      <Router hideNavBar>
         <Scene key="root">
           <Scene key="index" component={Index} title="Index" />
           <Scene key="item" component={Item} title="Item" />
