065b24e578932bb5081998932ec1725e4113d31d
Jin-Sih Lin <linpct@gmail.com>
2018-04-26 23:43:00 +0800

    add screen web

---
+++ template/src/screens/web.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground
+} from 'react-native';
+
+import {
+  Button
+} from 'react-native-elements';
+
+export default class Web extends Component {
+  render() {
+    return (
+      <Text>Web</Text>
+    )
+  }
+};
+
+styles = StyleSheet.create({});
