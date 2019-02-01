300c44677d331c566b2201874849d6ba1b4195f9
Jin-Sih Lin <linpct@gmail.com>
2019-02-01 21:39:10 +0800

    add notice to rename.sh

--- template/scripts/rename.sh
+++ template/scripts/rename.sh
@@ -3,4 +3,6 @@
 #react-native eject
 #react-native link
 
-echo 'please use react-native-rename <new_name> -b <new_bundle_id>' 
+echo 'Please use react-native-rename <new_name> -b <new_bundle_id>'
+echo '[NOTICE] remember to change <new_name> in any files in ./ios folder'
+
