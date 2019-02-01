833f4f4789850f24891ebfa1aaedf9cfb04e5726
Jin-Sih Lin <linpct@gmail.com>
2019-02-01 21:49:37 +0800

    sync rename.sh

--- template/scripts/rename.sh
+++ template/scripts/rename.sh
@@ -4,5 +4,5 @@
 #react-native link
 
 echo 'Please use react-native-rename <new_name> -b <new_bundle_id>'
-echo '[NOTICE] remember to change <new_name> in any files in ./ios folder'
+echo '[NOTICE] remember to change <new_name> in any files in ./ios and ./android folders'
 
