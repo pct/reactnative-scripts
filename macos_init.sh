ae1cf768edf12eae3022b95f44b317022e5c6a4b
Jin-Sih Lin <linpct@gmail.com>
2020-06-22 16:48:22 +0800

    feat: add macos_init.sh

---
+++ macos_init.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+
+if [ $# -eq 0 ]; then
+  echo 'Please input <project-name>'
+  exit
+fi
+
+PROJECT_NAME=$1
+BASEDIR=$(dirname "$0")
+
+echo 'create project:' $PROJECT_NAME '...'
+
+# === init ===
+npx react-native init $PROJECT_NAME
+echo "# Vim\n*~\n*swp" >> $PROJECT_NAME/.gitignore
+cp -rpf $BASEDIR/template/base/* $PROJECT_NAME
+
+mkdir -p $PROJECT_NAME/src
+cp -rpf $BASEDIR/template/default_src/* $PROJECT_NAME/src
+
+cd $PROJECT_NAME
+
+# === clean files ===
+rm -f App.js
+
+
+# === yarn ===
+yarn add lodash \
+  axios \
+  @react-navigation/native \
+  @react-navigation/stack \
+  react-native-gesture-handler \
+  react-native-reanimated \
+  react-native-screens \
+  react-native-elements \
+  react-native-vector-icons \
+  react-navigation-header-buttons \
+  react-native-storage \
+  react-native-keyboard-aware-scroll-view \
+  @react-native-community/geolocation \
+  @react-native-community/masked-view \
+  styled-components --save
+
+## install macos
+cd ios; pod install; cd -
+npx react-native-macos-init
+
+# === rm Base.lproj ===
+# 先不移除，可自己決定是否移除 LaunchScreen
+# rm -rf ios/$PROJECT_NAME/Base.lproj
+
+# === git ===
+git init; git add .; git commit -m 'init'
+
+# === done ===
+echo "\n\ndone!"
