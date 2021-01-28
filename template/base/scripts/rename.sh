72753019d9fceedab99e43ae266b971f31ec8487
Jin-Sih Lin <linpct@gmail.com>
2021-01-28 09:21:06 +0800

    feat: clean old scripts and add list_devices.sh

--- template/base/scripts/rename.sh
+++ template/base/scripts/rename.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
-#rm -rf ios android
-#react-native eject
-#react-native link
-
 echo 'Please use react-native-rename <new_name> -b <new_bundle_id>'
 echo '[NOTICE] remember to change <new_name> in any files in ./ios and ./android folders'
 
