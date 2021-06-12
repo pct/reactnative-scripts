fc606aa4778c20b1e32371a93a5fab816b4d77ce
Jin-Sih Lin <linpct@gmail.com>
2021-06-13 03:28:39 +0800

    fix: sample scripts

--- template/bottom_src/screens/pages.js
+++ template/bottom_src/screens/pages.js
@@ -4,12 +4,20 @@ import WebView from 'react-native-webview'
 import Config from 'react-native-config'
 
 const INJECTED_JAVASCRIPT = `
-setTimeout(function() { 
-  $('footer.u-sm-up-hide').remove()
-}, 5000);
+setTimeout(function() {
+  var footer = document.getElementsByClassName('u-sm-up-hide')[0];
 
-void(0);
+  if (footer) {
+    footer.remove();
+  }
+}, 1000);
+
+true;
 `;
+
+const onMessage = (event) => {
+  console.log(event)
+}
 
 const renderWebPage = (uri) => {
   return (
@@ -17,6 +25,7 @@ const renderWebPage = (uri) => {
       <WebView
         source={{ uri: Config.MAIN_URL + uri }}
         injectedJavaScript={INJECTED_JAVASCRIPT}
+        onMessage={(event) => { onMessage(event) }}
       />
     </SafeAreaView>
   )
