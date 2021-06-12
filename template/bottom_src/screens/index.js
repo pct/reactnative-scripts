051a46cf6690c63c5eedcdc3df141fa43d7d6b93
Jin-Sih Lin <linpct@gmail.com>
2021-06-13 04:00:48 +0800

    feat: add icons to bottom_src

--- template/bottom_src/screens/index.js
+++ template/bottom_src/screens/index.js
@@ -3,15 +3,21 @@ import { BottomNavigation } from 'react-native-paper'
 
 import { Home, Tags, Follow, Search, Login } from './pages'
 
+import HomeIcon from '../assets/icons/safari.png'
+import SearchIcon from '../assets/icons/search.png'
+import TagsIcon from '../assets/icons/tags.png'
+import FollowIcon from '../assets/icons/follow.png'
+import LoginIcon from '../assets/icons/login.png'
+
 export default function Index (navigation) {
   const [navIndex, setNavIndex] = useState(0)
 
   const routes = [
-    { key: 'home', title: '發現', color: '#3F51B5' },
-    { key: 'search', title: '搜尋', color: '#009688'  },
-    { key: 'tags', title: '標籤', color: '#795548'  },
-    { key: 'follow', title: '追蹤', color: '#607D8B' },
-    { key: 'login', title: '登入', color: '#3F51B5' },
+    { key: 'home', title: '發現', icon: HomeIcon, color: '#3F51B5' },
+    { key: 'search', title: '搜尋', icon: SearchIcon, color: '#009688'  },
+    { key: 'tags', title: '標籤', icon: TagsIcon, color: '#795548'  },
+    { key: 'follow', title: '追蹤', icon: FollowIcon, color: '#607D8B' },
+    { key: 'login', title: '登入', icon: LoginIcon, color: '#3F51B5' },
   ]
 
   const navigationState = {
