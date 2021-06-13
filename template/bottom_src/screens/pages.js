0c28a5a627c69baf2e2f9d43c8a5bee8c96c47c3
Jin-Sih Lin <linpct@gmail.com>
2021-06-13 12:00:41 +0800

    sync bottom_src template, add webview reload

--- template/bottom_src/screens/pages.js
+++ template/bottom_src/screens/pages.js
@@ -1,7 +1,12 @@
-import React, { Component } from 'react'
-import { SafeAreaView } from 'react-native'
+import React, { Component, useRef, useState } from 'react'
+import { View } from 'react-native'
 import WebView from 'react-native-webview'
 import Config from 'react-native-config'
+import styled from 'styled-components'
+import { Button } from 'react-native-paper'
+
+import ReloadIcon from '../assets/icons/reload.png'
+import BackIcon from '../assets/icons/back.png'
 
 const INJECTED_JAVASCRIPT = `
 setTimeout(function() {
@@ -20,16 +25,52 @@ const onMessage = (event) => {
 }
 
 const renderWebPage = (uri) => {
+  const webRef = useRef(null)
+  const pageUrl = Config.MAIN_URL + uri
+
+  const RELOAD_SCRIPT = `
+    window.location = '${pageUrl}';
+    true;
+  `
+
   return (
-    <SafeAreaView style={{ flex: 1 }}>
+    <View style={{ flex: 1 }}>
       <WebView
-        source={{ uri: Config.MAIN_URL + uri }}
+        ref={webRef}
+        source={{ uri: pageUrl }}
         injectedJavaScript={INJECTED_JAVASCRIPT}
         onMessage={(event) => { onMessage(event) }}
       />
-    </SafeAreaView>
+
+      <BottomView>
+        <IconButton
+          icon={BackIcon}
+          onPress={() => webRef.current.goBack() }
+          direction='rtl'
+        />
+        <IconButton
+          icon={ReloadIcon}
+          onPress={() => {
+            webRef.current.injectJavaScript(RELOAD_SCRIPT)
+          }}
+        />
+      </BottomView>
+    </View>
   )
 }
+
+const BottomView = styled.View`
+  position: absolute
+  bottom: 20
+  width: 100%
+  flex-direction: row
+  align-items: center
+  justify-content: space-between
+  background-color: transparent
+`
+
+const IconButton = styled(Button)`
+`
 
 export function Home() {
   return renderWebPage('/')
