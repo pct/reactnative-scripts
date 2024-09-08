1da496d3ac0d490e89aad50a2141a8070157b930
Jin-Sih Lin <linpct@gmail.com>
2024-09-08 19:31:31 +0800

    fix: brew install flipper

--- template/base/debugger.sh
+++ template/base/debugger.sh
@@ -2,5 +2,5 @@
 #open "rndebugger://set-debugger-loc?host=localhost&port=8081"
 
 # 因為 react-native 0.64 後跟 flipper 綁很深
-#brew cask install flipper
+#brew install flipper
 open /Applications/Flipper.app
