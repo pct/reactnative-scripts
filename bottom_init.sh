0efa1327b890f5362458757c6d7ad70e0979419d
Jin-Sih Lin <linpct@gmail.com>
2024-09-09 11:12:32 +0800

    fix: init 改用 npx @react-native-community/cli

--- bottom_init.sh
+++ bottom_init.sh
@@ -11,7 +11,7 @@ BASEDIR=$(dirname "$0")
 echo 'create project:' $PROJECT_NAME '...'
 
 # === init ===
-npx react-native@latest init $PROJECT_NAME
+npx @react-native-community/cli init $PROJECT_NAME
 echo "# Vim\n*~\n*swp" >> $PROJECT_NAME/.gitignore
 cp -rpf $BASEDIR/template/base/* $PROJECT_NAME
 
