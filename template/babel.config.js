c0bb055f5946606e7dfbb00ed0425d4b387ffc0d
Jin-Sih Lin <linpct@gmail.com>
2024-09-09 13:26:11 +0800

    fix: template/babel.config.js

--- template/babel.config.js
+++ template/babel.config.js
@@ -1,8 +1,7 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-      plugins: [
-      'react-native-paper/babel', 
-      'react-native-reanimated/plugin'
-      ],
-  },
+  presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    'react-native-paper/babel',
+    'react-native-reanimated/plugin',
+  ],
 }
